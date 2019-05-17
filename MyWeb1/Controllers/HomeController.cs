33a0cae122026a242e7a51a95942d0133f969472
Zuyan He <timmacy941@gmail.com>
2019-05-17 00:03:06 -0700

    Added Model to Home Controller

--- MyWeb1/Controllers/HomeController.cs
+++ MyWeb1/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MyWeb1.Models;
 
 namespace MyWeb1.Controllers
 {
@@ -10,6 +11,12 @@ namespace MyWeb1.Controllers
     {
         public ActionResult Index()
         {
+            var myData = new HomeViewModel
+            {
+                PhoneCount = 10,
+                UserCount = 15,
+                LastAcessDate = DateTime.Now
+            };
             return View();
         }
 
